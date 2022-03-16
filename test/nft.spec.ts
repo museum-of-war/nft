328e5def2d456c98802e81f46e08bedab07921d7
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2022-03-16 23:29:44 +0200

    add test for minting

--- test/nft.spec.ts
+++ test/nft.spec.ts
@@ -15,6 +15,8 @@ describe("Meta History", function () {
     const maxTokens = 100;
     const mintsPerWallet = 5;
     const initialCharityAddress = "0x165CD37b4C644C2921454429E7F9358d18A45e14";
+    const name = "Museum of History";
+    const symbol = "MH";
     let owner: SignerWithAddress,
         signer: SignerWithAddress,
         other: SignerWithAddress,
@@ -33,8 +35,7 @@ describe("Meta History", function () {
         Wallets = await FactoryW.deploy(owner.address, initialCharityAddress);
         WalletsWithOtherSigner = Wallets.connect(other);
 
-        MH = await FactoryMH.deploy(initialPrice, maxTokens,
-            "Museum of History", "MH",
+        MH = await FactoryMH.deploy(initialPrice, maxTokens, name, symbol,
             mintsPerWallet, Wallets.address, initialCharityAddress
         );
         MHWithOtherSigner = MH.connect(other);
@@ -63,5 +64,43 @@ describe("Meta History", function () {
         await Wallets.change_withdraw(withdraw.address);
 
         expect(await Wallets.view_withdraw()).to.be.equal(withdraw.address);
+    });
+
+    it("MH must be initialized", async () => {
+        expect(await MH.owner()).to.be.equal(owner.address);
+        expect(await MH.ukraineAddress()).to.be.equal(initialCharityAddress);
+        expect(await MH.view_Ukraine()).to.be.equal(initialCharityAddress); // looks like a duplicate
+        expect(await MH.name()).to.be.equal(name);
+        expect(await MH.symbol()).to.be.equal(symbol);
+        expect(await MH.price()).to.be.equal(initialPrice);
+    });
+
+    it("Only owner is allowed to unpause", async () => {
+        await expect(
+            MHWithOtherSigner.unpause()
+        ).to.be.revertedWith("ERROR");
+
+        const transaction = MH.unpause();
+
+        await expect(transaction).to.emit(MH, "Unpaused").withArgs(owner.address);
+    });
+
+    it("MH must mint", async () => {
+        const nonce = await MH.view_block_number();
+        const blockNumber = await ethers.provider.getBlockNumber();
+        expect(nonce.toNumber()).to.be.equal(blockNumber); // it is possible to get block number without adding function
+
+        const numberOfTokens = 1;
+        const price = await MH.price();
+
+        const messageHash = ethers.utils.solidityKeccak256(['address', 'uint256', 'uint256'],
+            [other.address, numberOfTokens, nonce]
+        );
+        const signature = await signer.signMessage(ethers.utils.arrayify(messageHash));
+
+        const transaction = MHWithOtherSigner.mint(signature, nonce, numberOfTokens, { value: price });
+
+        await expect(transaction).to.emit(MHWithOtherSigner, "Transfer")
+            .withArgs(constants.AddressZero, other.address, 1);
     });
 });
